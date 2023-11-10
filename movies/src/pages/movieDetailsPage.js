a70faccb26ebef7a2d9e0005aec03ef5b78c30cc
Dylan <dylanchai2003@gmail.com>
2023-11-10 13:12:29 +0000

    Updating movie page to use react query

--- movies/src/pages/movieDetailsPage.js
+++ movies/src/pages/movieDetailsPage.js
@@ -1,18 +1,32 @@
-import React, {useState, useEffect}  from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from 'react-router-dom';
 import MovieDetails from "../components/movieDetails/";
 import PageTemplate from "../components/templateMoviePage";
-import { getMovie } from "../api/tmdb-api";
+import { getMovie } from '../api/tmdb-api'
+import { useQuery } from "react-query";
+import Spinner from '../components/spinner'
 
 const MoviePage = (props) => {
   const { id } = useParams();
-  const [movie, setMovie] = useState(null);
+  const { data: movie, error, isLoading, isError } = useQuery(
+    ["movie", { id: id }],
+    getMovie
+  );
 
-  useEffect(() => {
-    getMovie(id).then((movie) => {
-      setMovie(movie);
-    });
-  }, [id]);
+  // Remove this useEffect block since it's redundant
+  // useEffect(() => {
+  //   getMovie(id).then((movie) => {
+  //     setMovie(movie);
+  //   });
+  // }, [id]);
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+
+  if (isError) {
+    return <h1>{error.message}</h1>;
+  }
 
   return (
     <>
