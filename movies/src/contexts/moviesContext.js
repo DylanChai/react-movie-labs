f0e13d4bf7f3269bad7c4715529f5965f0dd4a13
Dylan <dylanchai2003@gmail.com>
2023-11-15 14:22:18 +0000

    Stored reviews in the context

--- movies/src/contexts/moviesContext.js
+++ movies/src/contexts/moviesContext.js
@@ -4,7 +4,7 @@ export const MoviesContext = React.createContext(null);
 
 const MoviesContextProvider = (props) => {
   const [favorites, setFavorites] = useState( [] )
-
+  const [myReviews, setMyReviews] = useState({}); // Added state for review
   const addToFavorites = (movie) => {
     let newFavorites = [];
     if (!favorites.includes(movie.id)){
@@ -15,7 +15,8 @@ const MoviesContextProvider = (props) => {
     }
     setFavorites(newFavorites)
   };
-  
+
+  //console.log(myReviews);
   // We will use this function in a later section
   const removeFromFavorites = (movie) => {
     setFavorites( favorites.filter(
@@ -23,12 +24,17 @@ const MoviesContextProvider = (props) => {
     ) )
   };
 
+  const addReview = (movie, review) => { // Added handler for adding a review
+    setMyReviews({ ...myReviews, [movie.id]: review });
+  };
+
   return (
     <MoviesContext.Provider
       value={{
         favorites,
         addToFavorites,
-        removeFromFavorites
+        removeFromFavorites,
+        addReview,
       }}
     >
       {props.children}
